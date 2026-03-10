8f8f6148b66ef8aab45bdb0dcb670d66a9693726
sangram-jr <sangrambera58@gmail.com>
2026-03-10 16:56:17 +0530

    add join_room,leave_room and chat logic inside ws-backend using global array concept

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,39 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer,WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import "dotenv/config";
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
+
+interface User{
+  ws:WebSocket,
+  rooms:string[],
+  userId:string
+}
+
+//global array
+const users:User[]=[];
+
+
+
+//decode the token and get the useId from jwt
+function cheakUser(token:string):string|null{
+  try {
+    const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload;
+    if(typeof decoded=="string"){
+      return null;
+    }
+    if(!decoded || !decoded.userId){
+      return null;
+    }
+    return decoded.userId;
+  } catch (error) {
+    return null;
+  }
+  
+}
+
+
 
 wss.on('connection', function connection(ws,request) {
 
@@ -17,16 +48,62 @@ wss.on('connection', function connection(ws,request) {
     return;
   }
 
-  //decode the token
-  const decoded=jwt.verify(token,JWT_SECRET) as JwtPayload;
-  if(!decoded || !decoded.userId){
+  const userId=cheakUser(token);
+  if(!userId){
     ws.close();
     return;
   }
 
-  //get the message and send "pong" message
+  //push all the details into the users global array
+  users.push({
+    userId,
+    rooms:[],
+    ws
+  })
+
+  //get the message from user and send this message to server
   ws.on('message', function message(data) {
-    ws.send('pong');
+    //msg that server get from the user, that is string, we need to convert this in a object
+    const parseData=JSON.parse(data as unknown as string);
+
+    if(parseData.type==="join_room"){            //parseData->  {"type":"join_room","roomId":"red"}
+      //find the user from users global array
+      const user=users.find(x=>x.ws===ws);
+      if(!user){
+        return;
+      }
+      //push the roomId to the rooms of the users global array
+      user.rooms.push(parseData.roomId);
+    }
+
+
+    if(parseData.type==="leave_room"){          //parseData->  {"type":"leave_room","roomId":"red"}
+      //find the user
+      const user=users.find(x=>x.ws==ws);
+      if(!user){
+        return;
+      }
+      //delete the roomId from the rooms 
+      user.rooms=user?.rooms.filter(x=>x!==parseData.roomId)
+    }
+
+
+    if(parseData.type==="chat"){           //parseData->  {"type":"chat","message":"hi there","roomId":"red"}
+      const roomId=parseData.roomId;
+      const message=parseData.message;
+
+      //how many roomId include in the rooms , send them message.
+      users.forEach(user=>{
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type:"chat",
+            message:message,
+            roomId
+          }))
+        }
+      })
+    }
+
   });
 
   
