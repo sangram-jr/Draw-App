ceb794e49c6f09051a76325274a5af4e61c1554c
sangram-jr <sangrambera58@gmail.com>
2026-03-20 17:49:08 +0530

    add 3 icons on topbar

--- apps/excallidraw-frontend/components/Canvas.tsx
+++ apps/excallidraw-frontend/components/Canvas.tsx
@@ -1,5 +1,10 @@
 import { initDraw } from "@/draw";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { IconButton } from "./IconButton";
+import { Pencil,Circle,RectangleHorizontal } from 'lucide-react';
+
+
+type Shape='circle' | 'pencil' | 'rect';
 
 interface canvasProps{
     roomId:string,
@@ -7,6 +12,7 @@ interface canvasProps{
 }
 export function Canvas({roomId,socket}:canvasProps){
     const canvasRef=useRef<HTMLCanvasElement>(null);
+    const [selectedTool,setSelectedTool]=useState<Shape>('pencil');
         
     useEffect(()=>{
         if(canvasRef.current){
@@ -16,7 +22,37 @@ export function Canvas({roomId,socket}:canvasProps){
         
     },[canvasRef])
         
-    return <div>
-        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+    return <div style={{height:"100vh",overflow:"hidden"}}>
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+
+        <Topbar selectedTool={selectedTool} setSelectedTool={setSelectedTool} />
+    </div>
+}
+
+
+interface TopbarProps{
+    selectedTool:Shape,
+    setSelectedTool:(s:Shape)=>void
+}
+
+function Topbar({selectedTool,setSelectedTool}:TopbarProps){
+    return <div className="fixed top-10 left-10">
+        <div className="flex gap-2">
+            <IconButton 
+                onClick={()=>{setSelectedTool("pencil")}}
+                icon={<Pencil/>}
+                activated={selectedTool==="pencil"}
+            />
+            <IconButton 
+                onClick={()=>{setSelectedTool("circle")}}
+                icon={<Circle/>}
+                activated={selectedTool==="circle"}
+            />
+            <IconButton 
+                onClick={()=>{setSelectedTool("rect")}}
+                icon={<RectangleHorizontal/>}
+                activated={selectedTool==="rect"}
+            />
+        </div>
     </div>
 }
---
+++ apps/excallidraw-frontend/components/IconButton.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+interface IconButtonProps{
+    icon:ReactNode,
+    activated:boolean,
+    onClick:()=>void
+}
+export function IconButton({icon,activated,onClick}:IconButtonProps){
+    return <div onClick={onClick} className={`m-2 p-2 pointer rounded-full bg-black border hover:bg-gray ${activated ? "text-red-600" : "text-white"}`}>
+        {icon}
+    </div>
+}
