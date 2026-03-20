e6994b301899d0e979743e535f4ac4a88f206c8a
sangram-jr <sangrambera58@gmail.com>
2026-03-20 08:40:02 +0530

    shift rect logic in draw folder

--- apps/excallidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excallidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
 
 export default function Canvas(){
@@ -9,47 +10,7 @@ export default function Canvas(){
     useEffect(()=>{
         if(canvasRef.current){
             const canvas=canvasRef.current;
-            //get context or get shapes
-            const ctx=canvas.getContext("2d");
-
-            if(!ctx){
-                return;
-            }
-            //set canvas or screen black color
-            ctx.fillStyle="rgba(0,0,0)"
-            ctx.fillRect(0,0,canvas.width,canvas.height);
-
-            let clicked=false;
-            let startX=0;
-            let startY=0;
-
-            canvas.addEventListener("mousedown",(e)=>{
-                clicked=true;
-                startX=e.clientX;
-                startY=e.clientY;
-            })
-            canvas.addEventListener("mouseup",(e)=>{
-                clicked=false;
-                console.log(e.clientX);
-                console.log(e.clientY);
-                
-            })
-            canvas.addEventListener("mousemove",(e)=>{
-                if(clicked){
-                    //get height and width
-                    const width=e.clientX-startX;
-                    const height=e.clientY-startY;
-                    //clear rectangle
-                    ctx.clearRect(0,0,canvas.width,canvas.height);
-                    //set canvas or screen black color
-                    ctx.fillStyle="rgba(0,0,0,1)"
-                    ctx.fillRect(0,0,canvas.width,canvas.height);
-                    //set rectangle white color
-                    ctx.strokeStyle="rgba(255,255,255)"
-                    ctx.strokeRect(startX,startY,width,height);
-                }
-                
-            })
+            initDraw(canvas);
         }
 
     },[canvasRef])
---
+++ apps/excallidraw-frontend/draw/index.ts
@@ -0,0 +1,44 @@
+export function initDraw(canvas:HTMLCanvasElement){
+    //get context or get shapes
+    const ctx=canvas.getContext("2d");
+
+    if(!ctx){
+        return;
+    }
+    //set canvas or screen black color
+    ctx.fillStyle="rgba(0,0,0)"
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    let clicked=false;
+    let startX=0;
+    let startY=0;
+
+    canvas.addEventListener("mousedown",(e)=>{
+        clicked=true;
+        startX=e.clientX;
+        startY=e.clientY;
+    })
+    canvas.addEventListener("mouseup",(e)=>{
+        clicked=false;
+        console.log(e.clientX);
+        console.log(e.clientY);
+                
+    })
+    canvas.addEventListener("mousemove",(e)=>{
+        if(clicked){
+            //get height and width
+            const width=e.clientX-startX;
+            const height=e.clientY-startY;
+            //clear rectangle
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            //set canvas or screen black color
+            ctx.fillStyle="rgba(0,0,0,1)"
+            ctx.fillRect(0,0,canvas.width,canvas.height);
+            //set rectangle white color
+            ctx.strokeStyle="rgba(255,255,255)"
+            ctx.strokeRect(startX,startY,width,height);
+        }
+                
+    })
+        
+}
